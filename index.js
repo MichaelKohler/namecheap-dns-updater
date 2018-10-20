9511f50ab9709c8573531471ebacea2fb2ab5ed9
Michael Kohler <me@michaelkohler.info>
2018-10-20 15:45:34 +0200

    Register update interval

--- index.js
+++ index.js
@@ -13,18 +13,19 @@ try {
 
 const isValidConfig = checkConfig();
 if (!isValidConfig) {
-  throw new Error('INVALID_CONFIG: domain, tld, ttl, and at least one domain needs to be configured!');
+  throw new Error('INVALID_CONFIG: domain, tld, ttl, updateIntervalInMS, and at least one domain needs to be configured!');
 }
 
 debug('ALL_GOOD_START_UPDATE');
-updater.start(config);
+updater.registerInterval(config);
 
 function checkConfig() {
   debug('CHECK_CONFIG');
   const hasDomain = !!config.domain;
   const hasTLD = !!config.tld;
   const hasTTL = !!config.ttl;
+  const hasUpdateInterval = !!config.updateIntervalInMS;
   const hasDomains = config.domains && config.domains.length > 0;
 
-  return hasDomain && hasTLD && hasTTL && hasDomains;
+  return hasUpdateInterval && hasDomain && hasTLD && hasTTL && hasDomains;
 }
